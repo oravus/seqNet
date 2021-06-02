20b8689629a96f2493e9f8f53ca231d08210718f
oravus <s.garg@qut.edu.au>
2021-06-02 16:21:48 +1000

    download models

--- download.sh
+++ download.sh
@@ -1,4 +1,10 @@
+# download nordland-clean dataset
 wget -cO - https://cloudstor.aarnet.edu.au/plus/s/PK98pDvLAesL1aL/download > nordland-clean.zip
 mkdir -p ./data/
 unzip nordland-clean.zip -d ./data/
 rm nordland-clean.zip
+
+# download trained models
+wget -cO - https://cloudstor.aarnet.edu.au/plus/s/oMwpOzex5ld4nQq/download > models-nordland.zip
+unzip models-nordland.zip -d ./data/
+rm models-nordland.zip
