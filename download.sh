f7933a0f05c370b240877e4ad0aff5a0e19bf1ce
Sourav Garg <38741296+oravus@users.noreply.github.com>
2024-01-27 19:02:44 +1030

    auto download disabled, direct links provided

--- download.sh
+++ download.sh
@@ -1,15 +1,18 @@
+# Please download nordland-clean dataset [EDIT 27 Jan 2024: directly download from here: https://universityofadelaide.box.com/s/zkfk1akpbo5318fzqmtvlpp7030ex4up ]
+# Please download trained models [EDIT 27 Jan 2024: directly download from here: https://universityofadelaide.box.com/s/mp45yapl0j0by6aijf5kj8obt8ky0swk]
+
 # download nordland-clean dataset
-wget -cO - https://cloudstor.aarnet.edu.au/plus/s/PK98pDvLAesL1aL/download > nordland-clean.zip
-mkdir -p ./data/
-unzip nordland-clean.zip -d ./data/
-rm nordland-clean.zip
+#wget -cO - https://cloudstor.aarnet.edu.au/plus/s/PK98pDvLAesL1aL/download > nordland-clean.zip
+#mkdir -p ./data/
+#unzip nordland-clean.zip -d ./data/
+#rm nordland-clean.zip
 
 # download oxford descriptors
-wget -cO - https://cloudstor.aarnet.edu.au/plus/s/T0M1Ry4HXOAkkGz/download > oxford_2014-12-16-18-44-24_stereo_left.npy
-wget -cO - https://cloudstor.aarnet.edu.au/plus/s/vr21RnhMmOkW8S9/download > oxford_2015-03-17-11-08-44_stereo_left.npy
-mv oxford* ./data/descData/netvlad-pytorch/
+#wget -cO - https://cloudstor.aarnet.edu.au/plus/s/T0M1Ry4HXOAkkGz/download > oxford_2014-12-16-18-44-24_stereo_left.npy
+#wget -cO - https://cloudstor.aarnet.edu.au/plus/s/vr21RnhMmOkW8S9/download > oxford_2015-03-17-11-08-44_stereo_left.npy
+#mv oxford* ./data/descData/netvlad-pytorch/
 
 # download trained models
-wget -cO - https://cloudstor.aarnet.edu.au/plus/s/oMwpOzex5ld4nQq/download > models-nordland.zip
-unzip models-nordland.zip -d ./data/
-rm models-nordland.zip
+#wget -cO - https://cloudstor.aarnet.edu.au/plus/s/oMwpOzex5ld4nQq/download > models-nordland.zip
+#unzip models-nordland.zip -d ./data/
+#rm models-nordland.zip
